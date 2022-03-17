da2940542dba571ca6259700c1bc1e51be22dd05
Ryan Stich <55109747+RyanStich@users.noreply.github.com>
2022-03-17 03:52:00 -0700

    Creates base pages

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET Gardens page. */
+router.get('/gardens', function(req, res, next) {
+  res.render('gardens', { title: 'Express' });
+});
+
+
+/* GET Membership page. */
+router.get('/membership', function(req, res, next) {
+  res.render('members', { title: 'Express' });
+});
+
 module.exports = router;
