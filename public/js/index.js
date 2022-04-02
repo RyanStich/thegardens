01e4444afe7b84e11b9cb418b7a6aacc7abc95c8
Ryan Stich <55109747+RyanStich@users.noreply.github.com>
2022-04-01 17:21:29 -0700

    homepage

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+const clickx= document.getElementById('pencet');
+
+clickx.addEventListener('click', function(){
+  clickx.classList.toggle('Diam');
+});
+
+const navToggler = document.getElementById("backnav")
+
+const hamburger = document.getElementById("menu")
+console.log(hamburger)
+
+console.log(navToggler)
+
+hamburger.addEventListener('click', function(){
+    navToggler.classList.toggle('navback');
+  });
